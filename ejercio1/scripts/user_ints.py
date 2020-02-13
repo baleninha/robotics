433c9c629a75a329b1557d451f95cf22f43d76f3
baleninha <mariafozh@gmail.com>
2020-02-13 13:15:34 +0100

    ejercicio 1

---
+++ ejercio1/scripts/user_ints.py
@@ -0,0 +1,6 @@
+x = "y"
+while x == "y":
+
+    num = int(input("Input any number: "))
+    print(num)
+    x = str(input("Continue? (y/n): "))
---
+++ talker_py/scripts/user_ints.py
@@ -0,0 +1,6 @@
+x = "y"
+while x == "y":
+    print("Press n to exit")
+    num = int(input("Input any number: "))
+    print(num)
+    x = str(input("Continue? (y/n): "))
